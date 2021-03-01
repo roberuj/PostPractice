b1483f632bc700f628298ec43653401a58a604f8
fbanana <fbanana@LAPTOP-RL08QUJC>
2021-03-01 21:56:48 +0800

    unit testing mocking completed

--- spring-boot/src/test/java/com/example/springboot/PostMockTester.java
+++ spring-boot/src/test/java/com/example/springboot/PostMockTester.java
@@ -13,16 +13,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.NestedRuntimeException;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+
 
 import static org.mockito.Mockito.when;   // ...or...
 
 import java.io.File;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -31,6 +36,8 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 import com.example.domain.Comment;
@@ -40,7 +47,11 @@ import com.example.restClient.CommentClient;
 import com.example.restClient.PostClient;
 import com.example.service.PostsUserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.restpractice.UserException.UserConnectionException;
+import com.restpractice.UserException.UserEmptyException;
+import com.restpractice.UserException.UserFormatException;
 
+import ch.qos.logback.classic.Logger;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -60,6 +71,15 @@ public class PostMockTester {
 	private final static String postCommentJsonFile = "comment.json";
 	private final static String postNotExistentComment = "postNotExistentComment.json";
 	private final static String postCommentConnectionException = "postCommentConnectionException.json";
+	
+	@Value("${example.jsonPath}")
+	private String jsonPathOutput;
+	@Value("${example.postJsonFile}")
+	private String postJsonFileOutput;
+	@Value("${example.postXmlFile}")
+	private String postXmlFile;
+	@Value("${example.xmlPath}")
+	private String xmlPath;
 	
 	@Value("${example.baseUrl}")
 	private String baseUrl;
@@ -111,6 +131,12 @@ public class PostMockTester {
 	@Mock
 	private WebClient.ResponseSpec responseSpecExceptionCommentMock;
 	
+	@SuppressWarnings("rawtypes")
+	@Mock
+	private WebClient.RequestHeadersSpec requestHeadersSpecThrowExceptionPostMock;
+	@Mock
+	private WebClient.ResponseSpec responseSpecThrowExceptionPostMock;
+	
 	@Autowired
 	PostsUserService postsUserService;
 	
@@ -145,7 +171,7 @@ public class PostMockTester {
 		Collections.addAll(postList, postArray);
 		Collections.addAll(commentList, commentArray);
 		Collections.addAll(postWithEmptyCommentList, postWithEmptyCommentArray);
-		Collections.addAll(postWithConnExceptionList, postWithEmptyCommentArray);
+		Collections.addAll(postWithConnExceptionList, postWithConnectException);
 	}
 	@SuppressWarnings("unchecked")
 	@BeforeEach
@@ -188,8 +214,51 @@ public class PostMockTester {
 	    when(responseSpecEmptyCommentMock.bodyToMono(new ParameterizedTypeReference<List<Comment>>() {}))
 	      .thenReturn(Mono.just(new ArrayList<Comment>()));
 	    
+	  //get Exception when we want to get the comment.
+	   
+		
+	    when(requestHeadersUriSpecMock.uri(baseUrl+urlSuffix, 2))
+        .thenReturn(requestHeadersSpecExceptionPostMock);
+		when(requestHeadersSpecExceptionPostMock.retrieve())
+        .thenReturn(responseSpecExceptionPostMock);
+		when(responseSpecExceptionPostMock.bodyToMono(new ParameterizedTypeReference<List<Post>>() {}))
+        .thenReturn(Mono.just(postWithConnExceptionList));
+	    when(requestHeadersUriSpecMock.uri(baseUrl+urlCommentSuffix, 2))
+		.thenReturn(requestHeadersSpecExceptionCommentMock);
+	    when(requestHeadersSpecExceptionCommentMock.retrieve())
+	      .thenReturn(responseSpecExceptionCommentMock);
+	    when(responseSpecExceptionCommentMock.bodyToMono(new ParameterizedTypeReference<List<Comment>>() {}))
+	    .thenReturn(Mono.error(new WebClientRequestException(new UnknownHostException(), null, null, null)));
+	   
+	    when(requestHeadersUriSpecMock.uri(baseUrl+urlSuffix, 4))
+	    .thenReturn(requestHeadersSpecThrowExceptionPostMock);
+		when(requestHeadersSpecThrowExceptionPostMock.retrieve())
+        .thenReturn(responseSpecThrowExceptionPostMock);
+		when(responseSpecThrowExceptionPostMock.bodyToMono(new ParameterizedTypeReference<List<Post>>() {}))
+	    .thenReturn(Mono.error(new WebClientRequestException(new UnknownHostException(), null, null, null)));
 	    
 	    
+	}
+	
+	@Test
+	@DisplayName("Testing we are controlling the connection problems when we are calling for getting posts by user")
+	public void testConnectionProblemwithPosts() {
+		String parameter = "4";
+		
+		Throwable throable = assertThrows(Exception.class, ()-> postsUserService.getCommentsByPostUser(parameter).toStream().collect(Collectors.toList()));
+		assertNotEquals(throable.getCause(), null);
+		assertTrue(throable.getCause() instanceof UserConnectionException);
+		assertEquals("The host or the internet connection is down", throable.getCause().getMessage());
+	}
+	
+	@Test
+	@DisplayName("Testing we are controlling the connection problems when we are extracting the comments of a post")
+	public void testConnectionProblemwithComents() {
+		String parameter = "2";
+		Throwable throable = assertThrows(Exception.class, ()-> postsUserService.getCommentsByPostUser(parameter).toStream().collect(Collectors.toList()));
+		assertNotEquals(throable.getCause(), null);
+		assertTrue(throable.getCause() instanceof UserConnectionException);
+		assertEquals("The host or the internet connection is down", throable.getCause().getMessage());
 	}
 	
 	@Test
@@ -261,5 +330,36 @@ public class PostMockTester {
       assertEquals(posts.size(),1);
 	}
 	
+	@Test
+	@DisplayName("Verify the json file existence")
+	public void testJsonFileExistence() {
+		File file = new File(jsonPathOutput+postJsonFileOutput);
+		assertEquals(file.exists(),true);
+		assertEquals(file.length()>0,true);
+	}
+	
+	@Test
+	@DisplayName("Verify the xml file existence")
+	public void testXmlFileExistence() {
+		File file = new File(xmlPath+postXmlFile);
+		assertEquals(file.exists(),true);
+		assertEquals(file.length()>0,true);
+	}
+	
+	@Test
+	@DisplayName("Testing with a null value as a user and verify the system doesn't crash and an expected UserException is cought")
+	public void testUserNullValue() {
+		Throwable throable = assertThrows(UserEmptyException.class, ()-> postsUserService.getCommentsByPostUser(null).toStream());
+		assertEquals("user cannot be empty", throable.getMessage());
+	}
+	
+	@Test
+	@DisplayName("Testing with a wrong parameter format and verify the system doesn't crash and an expected UserException is cought")
+	public void testWrongParameterFormat() {
+		String parameter = "3a";
+		Throwable throable = assertThrows(UserFormatException.class, ()-> postsUserService.getCommentsByPostUser(parameter).toStream());
+		assertEquals("User format not valid, " + parameter, throable.getMessage());
+	}
+
 	
 }
